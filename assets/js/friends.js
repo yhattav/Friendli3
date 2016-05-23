3d882a0ba8a4184b56629e6db67a355e6fd59a61
yhattav <yhattav@gmail.com>
2016-05-23 14:47:41 +0300

    works with db - very basic functionality

---
+++ assets/js/friends.js
@@ -0,0 +1,46 @@
+/*$(document).ready(function () {
+
+  if (document.location.href.indexOf('email') < 0 && document.location.pathname != '/') {
+    document.location.href = '/';
+  }*/
+
+  $('#createFriend').submit(function (e) {
+    e.preventDefault();
+    $.ajax({
+      url: '/friends/create',
+      type: 'post',
+      data: $(this).serialize(),
+      success: function () {
+        document.location.reload();
+      }
+    });
+  });
+
+  $('.deleteFriend').click(function (e) {
+    e.preventDefault();
+    $.ajax({
+      url: $(this).attr('action'),
+      type: 'delete',
+      data: $(this).serialize(),
+      success: function () {
+        document.location.reload();
+      }
+    });
+  });
+
+  $('.updateFriend').click(function (e) {
+    e.preventDefault();
+    $.ajax({
+      url: $(this).attr('action'),
+      type: 'put',
+      data: $(this).serialize(),
+      success: function () {
+        document.location.reload();
+      }
+    });
+  }); 
+// });
+
+function md5(text) {
+
+}
